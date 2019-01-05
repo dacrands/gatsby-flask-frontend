640d80303711dd96d1fbbe1c18c4bf03d28cebc6
David Crandall <davecrands@gmail.com>
2019-01-05 01:00:37 -0500

    Nav links based on auth

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,73 @@
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
+import  React from 'react'
 
-const Header = ({ siteTitle }) => (
-  <div
+import { getUser } from '../utils/auth'
+
+const Header = ({ siteTitle }) => {
+  if (getUser() === "no one") {    
+    // navigate('/login/')
+    return <div
     style={{
       // background: `rebeccapurple`,
+      color: `#dadada`,
       marginBottom: `1.45rem`,
       padding: `1.45rem 1.0875rem`
     }}
   >
-    <nav>
-      <a href="http://localhost:5000/logout">Logout</a>        
+    <nav className="navbar">
+      <Link
+          to="/login/"
+          activeStyle= {{          
+            color: "orange"
+          }}>
+          Login
+        </Link>
+      </nav>    
+    </div>
+  }
+  return <div
+    style={{
+      // background: `rebeccapurple`,
+      color: `#dadada`,
+      marginBottom: `1.45rem`,
+      padding: `1.45rem 1.0875rem`
+    }}
+  >
+    <nav className="navbar">
+    <Link
+        to="/"
+        activeStyle= {{
+          color: "orange"
+        }}>
+        Home
+      </Link>   
+      {` `}     
+      <Link
+        to="/dash/"
+        activeStyle= {{
+          color: "orange"
+        }}>
+        Notes
+      </Link>      
+      {` `}
+      <Link
+        to="/logout/"
+        activeStyle= {{
+          color: "orange"
+        }}>
+        Logout
+      </Link>
     </nav>
   </div>
-)
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `David Crandall`,
 }
 
 export default Header
